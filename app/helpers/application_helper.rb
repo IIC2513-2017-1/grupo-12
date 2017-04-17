2af9ad87b2c88912f9061c2419df8b16bce629a1
vjfuenzalida <vjfuenzalida@uc.cl>
2017-04-17 13:46:03 -0300

    User model created + views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'Dreamfunder'
+    if page_title.empty?
+      base_title
+    else
+      base_title + ' | ' + page_title
+    end
+  end
+
+  def active_class(*args)
+    if !args[2].nil?
+      current_page?(controller: args[0], action: args[1], id: args[2]) ? 'active' : ''
+    else
+      current_page?(controller: args[0], action: args[1]) ? 'active' : ''
+    end
+  end
 end
