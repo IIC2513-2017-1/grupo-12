ac040f8d47baa19c828a6d6433494ae7da444f7a
vjfuenzalida <vjfuenzalida@uc.cl>
2017-04-17 14:07:57 -0300

    Added User seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  # Password implementation
+  # Password implementation (includes authenticate method)
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(firstname: 'Vicente', lastname: 'Fuenzalida',
+             email: 'vjfuenzalida@uc.cl', birthdate: Date.parse('13-10-1994'),
+             password: '123123', password_confirmation: '123123')
+
+User.create(firstname: 'Juan Pablo', lastname: 'Jofré', email: 'jbjofre@uc.cl',
+            birthdate: Date.parse('28-5-1994'),
+            password: '123123', password_confirmation: '123123')
+
+15.times do |n|
+  firstname = Faker::Name.unique.first_name
+  lastname = Faker::Name.unique.last_name
+  birthday = Faker::Date.birthday(18, 45)
+  email = "mail-#{n + 1}@mail.com"
+  password = '123123'
+  User.create!(firstname: firstname, lastname: lastname, email: email,
+               password: password, birthdate: birthday,
+               password_confirmation: password)
+end
