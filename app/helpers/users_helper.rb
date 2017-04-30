64de183a3b8a4787f859954432aad2558eef407f
vjfuenzalida <vjfuenzalida@uc.cl>
2017-04-30 03:49:30 -0300

    styled navbar and made minor changes to user show view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,14 @@
 module UsersHelper
   # Returns the Gravatar for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, size, **kwargs)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.fullname, class: 'gravatar')
+    default_url = 'https://grupo-12.herokuapp.com/default_avatar.png'
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
+    if kwargs[:class]
+      kwargs[:class] += ' gravatar img-circle'
+      return image_tag(gravatar_url, alt: user.fullname, class: kwargs[:class])
+    else
+      image_tag(gravatar_url, alt: user.fullname, class: 'gravatar img-circle')
+    end
   end
 end
