fa189ae98e6559df455729b6a4c2e606634ec80d
JUAN PABLO JOFRÉ <jbjofre@uc.cl>
2017-04-30 15:00:19 -0300

    project validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,17 @@
 class Project < ApplicationRecord
+  validates :brief, presence: true, allow_blank: false
+  validates :description, presence: true, allow_blank: false
+  validates :funding_duration, presence: true, allow_blank: false
+  validates :funding_goal, presence: true, allow_blank: false
   belongs_to :user
   has_many :comments
   has_many :donations
   has_many :passive_project_relationships, class_name:  "ProjectRelationship",
                                 foreign_key: "saved_id",
+                                validates :brief, presence: true, allow_blank: false
+  validates :description, presence: true, allow_blank: false
+  validates :funding_duration, presence: true, allow_blank: false
+  validates :funding_goal, presence: true, allow_blank: false
                                 dependent:   :destroy
   has_many :savers, through: :passive_project_relationships, source: :saver
 
